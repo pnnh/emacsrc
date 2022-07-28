611e3043aa517fae744b14185fd3ab6c8ef71db7
larry <xspanni@gmail.com>
2022-07-28 10:05:48 +0800

    增加markdown相关支持

--- packages/other.el
+++ packages/other.el
@@ -1,3 +1,7 @@
 ;; 这里存放其它小的插件配置合集
 (message "开始配置其它插件")
 ;; 下方可以防止其它插件的配置
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown"))
