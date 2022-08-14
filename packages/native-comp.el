565593b51da6d6cdce7056c34f3613b63f941bf1
larry <xspanni@gmail.com>
2022-08-14 13:37:50 +0800

    处理native-comp兼容问题，mac M1下不支持，此时不执行初始化

--- packages/native-comp.el
+++ packages/native-comp.el
@@ -1,20 +1,27 @@
+
+
+(defun init-compile ()
+  "执行native compile"
+  (message "Native compilation is available")
+  
+  (setq package-native-compile t)
+  (setq native-comp-deferred-compilation t)
+
+  ;; 下面是手动编译所有包的语法，会阻塞掉后续代码
+  ;; (native-comp-async "~/.emacs.d/elpa" 2 t)
+
+  ;; block until native compilation has finished
+  (while (or comp-files-queue
+              (> (comp-async-runnings) 0))
+      (message "waiting for compile finish...")
+      (sleep-for 1))
+)
+
 (if (and (fboundp 'native-comp-available-p)
        (native-comp-available-p))
-  (message "Native compilation is available")
+  (init-compile)
 (message "Native complation is *not* available"))
 
-(if (functionp 'json-serialize)
-  (message "Native JSON is available")
-(message "Native JSON is *not* available"))
-
-(setq package-native-compile t)
-(setq native-comp-deferred-compilation t)
-
-;; 下面是手动编译所有包的语法，会阻塞掉后续代码
-;; (native-comp-async "~/.emacs.d/elpa" 2 t)
-
-;; block until native compilation has finished
-(while (or comp-files-queue
-            (> (comp-async-runnings) 0))
-    (message "waiting for compile finish...")
-    (sleep-for 1))
+;; (if (functionp 'json-serialize)
+;;   (message "Native JSON is available")
+;; (message "Native JSON is *not* available"))
